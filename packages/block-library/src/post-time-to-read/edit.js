c19c3dae7d5f352ce28fe27dd3126f63220481e3
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2022-08-19 14:06:15 +0900

    Create block base

---
+++ packages/block-library/src/post-time-to-read/edit.js
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import {
+	AlignmentControl,
+	BlockControls,
+	useBlockProps,
+} from '@wordpress/block-editor';
+
+function PostTimeToReadEdit( { attributes, setAttributes } ) {
+	const { textAlign } = attributes;
+	const blockProps = useBlockProps( {
+		className: classnames( {
+			[ `has-text-align-${ textAlign }` ]: textAlign,
+		} ),
+	} );
+
+	return (
+		<>
+			<BlockControls group="block">
+				<AlignmentControl
+					value={ textAlign }
+					onChange={ ( nextAlign ) => {
+						setAttributes( { textAlign: nextAlign } );
+					} }
+				/>
+			</BlockControls>
+			<div { ...blockProps }>Post Time To Read</div>
+		</>
+	);
+}
+
+export default PostTimeToReadEdit;
