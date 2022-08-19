1483a3ecc68250aa6604e037da165c61aabdb2f6
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2022-08-19 14:22:35 +0900

    Add wordCount

--- packages/block-library/src/post-time-to-read/edit.js
+++ packages/block-library/src/post-time-to-read/edit.js
@@ -6,14 +6,45 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
+import { useSelect } from '@wordpress/data';
 import {
 	AlignmentControl,
 	BlockControls,
 	useBlockProps,
 } from '@wordpress/block-editor';
+import { store as editorStore } from '@wordpress/editor';
+// eslint-disable-next-line no-unused-vars
+import { __, _x } from '@wordpress/i18n';
+import { count as wordCount } from '@wordpress/wordcount';
+
+/**
+ * Average reading rate - based on average taken from
+ * https://irisreading.com/average-reading-speed-in-various-languages/
+ * (Characters/minute used for Chinese rather than words).
+ *
+ * @type {number} A rough estimate of the average reading rate across multiple languages.
+ */
+const AVERAGE_READING_RATE = 189;
 
 function PostTimeToReadEdit( { attributes, setAttributes } ) {
 	const { textAlign } = attributes;
+
+	const content = useSelect(
+		( select ) => select( editorStore ).getEditedPostAttribute( 'content' ),
+		[]
+	);
+
+	/*
+	 * translators: If your word count is based on single characters (e.g. East Asian characters),
+	 * enter 'characters_excluding_spaces' or 'characters_including_spaces'. Otherwise, enter 'words'.
+	 * Do not translate into your own language.
+	 */
+	const wordCountType = _x( 'words', 'Word count type. Do not translate!' );
+	// eslint-disable-next-line no-unused-vars
+	const minutesToRead = Math.round(
+		wordCount( content, wordCountType ) / AVERAGE_READING_RATE
+	);
+
 	const blockProps = useBlockProps( {
 		className: classnames( {
 			[ `has-text-align-${ textAlign }` ]: textAlign,
